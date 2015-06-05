f0587ce76a837f82f0fa756ca4bd3b1bd352f74a
Micah Merswolke <mecoking@icloud.com>
2015-06-04 22:54:04 -0400

    Added a world that tracks positions
    Will manage physics with these points and eventually track entities :D

---
+++ World.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ifElse {
+	public class World {
+		public static int seed = 0;
+		public static Random random;
+		public static Dictionary<Point, int> elevationMap = new Dictionary<Point, int> ();
+
+		public static void generate () {
+			random = new Random (seed);
+			for (int elv = 0; elv < random.Next (10, 16); elv++) {
+				getElevationAt (new Point (random.Next (-100, 101), random.Next (-100, 101)));
+			}
+		}
+
+		public static int getElevationAt (Point pt) {
+			if (!elevationMap.ContainsKey (pt)) {
+				Point neighbour = pt.closestNeighbour ();
+				int distance = pt.distanceFrom (neighbour );
+				int elevation = random.Next (elevationMap [neighbour] - distance, elevationMap [neighbour] + distance);
+				elevationMap.Add (pt, elevation);
+			}
+			return elevationMap [pt];
+		}
+		public static bool waterIsAt (Point pt) {
+			return false;
+		}
+	}
+}
+
