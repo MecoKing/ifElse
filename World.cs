c191b55a80f20d08033f38e8d5f742ed8d2fb85c
Micah Merswolke <mecoking@icloud.com>
2015-06-05 15:12:54 -0400

    Added seed-based terrain generation
    Good enough for now.

--- World.cs
+++ World.cs
@@ -3,23 +3,35 @@ using System.Collections.Generic;
 
 namespace ifElse {
 	public class World {
-		public static int seed = 0;
+		public static int seed = 1234567890;
 		public static Random random;
 		public static Dictionary<Point, int> elevationMap = new Dictionary<Point, int> ();
 
 		public static void generate () {
 			random = new Random (seed);
-			for (int elv = 0; elv < random.Next (10, 16); elv++) {
-				getElevationAt (new Point (random.Next (-100, 101), random.Next (-100, 101)));
+			elevationMap.Add (new Point (0, 0), random.Next (-5, 6));
+			for (int y = -5; y < 6; y++) {
+				for (int x = -5; x < 6; x++) {
+					int localElevation = getElevationAt (new Point (x, y));
+					if (localElevation < 0)
+						Console.Write ("{0} ", localElevation);
+					else
+						Console.Write (" {0} ", localElevation);
+				}
+				Console.WriteLine ();
 			}
 		}
 
 		public static int getElevationAt (Point pt) {
 			if (!elevationMap.ContainsKey (pt)) {
-				Point neighbour = pt.closestNeighbour ();
-				int distance = pt.distanceFrom (neighbour );
-				int elevation = random.Next (elevationMap [neighbour] - distance, elevationMap [neighbour] + distance);
-				elevationMap.Add (pt, elevation);
+				int totalWeight = 0;
+				int totalElevation = 0;
+				foreach (Point neighbour in elevationMap.Keys) {
+					totalWeight += pt.distanceFrom (neighbour);
+					totalElevation += elevationMap [neighbour] * pt.distanceFrom (neighbour);
+				}
+				int[] modFactor = new int[] { 0, 0, 1, 1, 1, 2, -1, -1, -1, -2 };
+				elevationMap.Add (pt, (totalElevation / totalWeight) + modFactor [random.Next (modFactor.Length)]);
 			}
 			return elevationMap [pt];
 		}
