502f2c63d9bb50eb51fa06788bc90f2eca51e4f9
Micah Merswolke <mecoking@icloud.com>
2015-06-15 08:02:37 -0400

    Documentation

--- World.cs
+++ World.cs
@@ -8,6 +8,7 @@ namespace ifElse {
 		public static Random random;
 		public static Dictionary<Point, int> elevationMap = new Dictionary<Point, int> ();
 
+		//Generates a random world from the given seed
 		public static void generate () {
 			random = new Random (seed);
 			elevationMap.Add (new Point (0, 0), random.Next (-5, 6));
@@ -24,6 +25,8 @@ namespace ifElse {
 			}
 		}
 
+		//Returns the elevation value at a certain point.
+		//If no elevation point exists yet, it generates a new one.
 		public static int getElevationAt (Point pt) {
 			if (!elevationMap.ContainsKey (pt)) {
 				int totalWeight = 0;
@@ -33,6 +36,7 @@ namespace ifElse {
 					totalElevation += elevationMap [neighbour] * pt.distanceFrom (neighbour);
 				}
 				int[] modFactor = new int[] { 0, 0, 1, 1, 1, 2, -1, -1, -1, -2 };
+				//Add a new terrain point with the weighted average elevation + a random factor.
 				elevationMap.Add (pt, (totalElevation / totalWeight) + modFactor [random.Next (modFactor.Length)]);
 			}
 			return elevationMap [pt];
