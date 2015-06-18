f6ae97ead96c433b17980626d8f2cb54b371862c
Micah Merswolke <mecoking@icloud.com>
2015-06-18 15:00:36 -0400

    Made the world track tiles instead of points

--- World.cs
+++ World.cs
@@ -6,16 +6,14 @@ namespace ifElse {
 		public static int seed = 1234567890;
 		public static int size = 5;
 		public static Random random;
-		public static Dictionary<Point, int> elevationMap = new Dictionary<Point, int> ();
-		public static List<Point> waterSprings = new List<Point> ();
+		public static List<Tile> map = new List<Tile> ();
 
 		//Generates a random world from the given seed
 		public static void generate () {
 			random = new Random (seed);
-			for (int i = 0; i < random.Next (4, 9); i++) {
-				waterSprings.Add (new Point (random.Next (-size, size+1), random.Next (-size, size+1)));
-			}
-			elevationMap.Add (new Point (0, 0), random.Next (-5, 6));
+			//Add a starting elevation point at (0, 0) with a randomized height
+			map.Add (new Tile (new Point (0, 0), random.Next (-5, 6)));
+			//for all coordinates in the giving size
 			for (int y = -size; y < size+1; y++) {
 				for (int x = -size; x < size+1; x++) {
 					int localElevation = getElevationAt (new Point (x, y));
@@ -31,27 +29,32 @@ namespace ifElse {
 
 		//Returns the elevation value at a certain point.
 		//If no elevation point exists yet, it generates a new one.
-		public static int getElevationAt (Point pt) {
-			if (!elevationMap.ContainsKey (pt)) {
+		public static int getElevationAt (Point location) {
+			if (!tileExistsAtPoint (location)) {
 				int totalWeight = 0;
 				int totalElevation = 0;
-				foreach (Point neighbour in elevationMap.Keys) {
-					totalWeight += pt.distanceFrom (neighbour);
-					totalElevation += elevationMap [neighbour] * pt.distanceFrom (neighbour);
+				foreach (Tile neighbour in map) {
+					int distance = location.distanceFrom (neighbour.position);
+					totalWeight += distance;
+					totalElevation += neighbour.elevation * distance;
 				}
 				int[] modFactor = new int[] { 0, 0, 1, 1, 1, 2, -1, -1, -1, -2 };
 				//Add a new terrain point with the weighted average elevation + a random factor.
-				elevationMap.Add (pt, (totalElevation / totalWeight) + modFactor [random.Next (modFactor.Length)]);
+				map.Add (new Tile(location, (totalElevation / totalWeight) + modFactor [random.Next (modFactor.Length)]));
 			}
-			return elevationMap [pt];
+			return tileAtPoint (location).elevation;
 		}
 
-		public static bool waterIsAt (Point pt) {
-			if (waterSprings.Contains (pt))
+		public static Tile tileAtPoint (Point pt) {
+			foreach (Tile tl in map) {
+				if (tl.position.x == pt.x && tl.position.y == pt.y)
+					return tl;
+			}
+			return null;
+		}
+		public static bool tileExistsAtPoint (Point pt) {
+			if (tileAtPoint (pt) != null)
 				return true;
 			else
 				return false;
 		}
-	}
-}
-
