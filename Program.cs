96dc411114b688ea5caceac4e86bea22c34f01e5
Micah Merswolke <mecoking@icloud.com>
2015-06-18 15:01:14 -0400

    Added methods to print elevation and water

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ namespace ifElse {
 	class Program {
 		public static void Main (string[] args) {
 			World.generate ();
+
+			World.printElevationMapAboutPoint (new Point (0, 0));
+			Console.WriteLine ();
+			World.printWaterMapAboutPoint (new Point (0, 0));
+			Console.WriteLine ();
 		}
 	}
 }
--- World.cs
+++ World.cs
@@ -10,20 +10,16 @@ namespace ifElse {
 
 		//Generates a random world from the given seed
 		public static void generate () {
+			//create a new random object with the world's seed
 			random = new Random (seed);
 			//Add a starting elevation point at (0, 0) with a randomized height
 			map.Add (new Tile (new Point (0, 0), random.Next (-5, 6)));
 			//for all coordinates in the giving size
 			for (int y = -size; y < size+1; y++) {
 				for (int x = -size; x < size+1; x++) {
-					int localElevation = getElevationAt (new Point (x, y));
-
-					if (localElevation < 0)
-						Console.Write ("{0} ", localElevation);
-					else
-						Console.Write (" {0} ", localElevation);
+					Point localPoint = new Point (x, y);
+					getElevationAt (localPoint);
 				}
-				Console.WriteLine ();
 			}
 		}
 
@@ -58,3 +54,52 @@ namespace ifElse {
 			else
 				return false;
 		}
+
+		public static void printElevationMapAboutPoint (Point origin) {
+			for (int y = origin.y-size; y < origin.y+size + 1; y++) {
+				for (int x = origin.x-size; x < origin.x+size + 1; x++) {
+					Point localPoint = new Point (x, y);
+					if (tileExistsAtPoint (localPoint)) {
+						int localElevation = tileAtPoint (localPoint).elevation;
+						if (localElevation < -1)
+							Console.ForegroundColor = ConsoleColor.Blue;
+						else if (localElevation < 0)
+							Console.ForegroundColor = ConsoleColor.Cyan;
+						else if (localElevation < 2)
+							Console.ForegroundColor = ConsoleColor.Green;
+						else if (localElevation < 4)
+							Console.ForegroundColor = ConsoleColor.Yellow;
+						else
+							Console.ForegroundColor = ConsoleColor.Red;
+
+						Console.Write ("# ");
+					} else
+						Console.Write ("  ");
+				}
+				Console.WriteLine ();
+			}
+		}
+		public static void printWaterMapAboutPoint (Point origin) {
+			for (int y = origin.y-size; y < origin.y+size + 1; y++) {
+				for (int x = origin.x-size; x < origin.x+size + 1; x++) {
+					Point localPoint = new Point (x, y);
+					if (tileExistsAtPoint (localPoint)) {
+						Tile localTile = tileAtPoint (localPoint);
+						if (localTile.isWaterSpring) {
+							Console.ForegroundColor = ConsoleColor.Blue;
+							Console.Write ("o ");
+						} else if (localTile.hasWater ()) {
+							Console.ForegroundColor = ConsoleColor.Blue;
+							Console.Write ("~ ");
+						} else {
+							Console.ForegroundColor = ConsoleColor.Green;
+							Console.Write ("# ");
+						}
+					} else
+						Console.Write ("  ");
+				}
+				Console.WriteLine ();
+			}
+		}
+	}
+}
